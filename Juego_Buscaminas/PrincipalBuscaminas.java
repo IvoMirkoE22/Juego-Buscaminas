9c0982de9bfd00a157f2d301ace31e08c74cc5fa
IvoMirkoE22 <ivomirkoelian20@gmail.com>
2025-06-23 15:53:13 -0300

    Agregada clase PrincipalBuscaminas con método main

---
+++ Juego_Buscaminas/PrincipalBuscaminas.java
@@ -0,0 +1,24 @@
+
+/**
+ * Esta clase es la que arranca todo el programa.
+ * Tiene el método main , que es el primer método
+ * que se ejecuta cuando se corra el proyecto
+ * 
+ * @author (Ivo Narváez) 
+ * @version (0.1)
+ */
+public class PrincipalBuscaminas
+{
+    //el args como parámetro en el main no se usa
+    //pero se debe poner ya que funciona como la
+    //firma estandar del método main en Java
+    public static void main(String args[]){
+    //Crea una instancia de la clase Buscaminas con new Buscaminas()
+    Buscaminas buscaminas = new Buscaminas();
+    
+    //llamamos al método inciarJuego(), que prepara el tablero,pondra minas
+    //y deja el juego listo para empezar.
+    buscaminas.inciarJuego();
+    
+    }
+}
